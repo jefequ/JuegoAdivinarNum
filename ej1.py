c7745920f294f304d849813bb911e2f633953fc7
jefequ <jefequ@yahoo.com>
2021-11-17 20:43:11 -0600

    inicio proy prueba git con juego adivinanza

---
+++ ej1.py
@@ -0,0 +1,39 @@
+import sys
+from random import randint
+
+#argumentos nombre, apellido num menor y num mayor (rango de número a adivinar)
+filename = sys.argv[0]
+first = sys.argv[1]
+last = sys.argv[2]
+print(f'filename: {filename}')
+print(f'hiii! {first} {last}')
+
+
+
+num_gen = randint(1, 10)
+lim_inf= int(sys.argv[3])
+lim_sup = int(sys.argv[4])
+num_gen = randint(lim_inf, lim_sup)
+num_opor = 3
+
+while num_opor > 0:
+    try:
+        print(num_gen)
+        guess = int(input(f'Tienes {num_opor} oportunidades para adivinar un num entre {lim_inf}-{lim_sup}: '))
+        if lim_inf <= guess <= lim_sup:
+            if guess == num_gen:
+                print('you won')
+                break
+            else:
+                num_opor -= 1
+                if num_opor <= 0:
+                    print(f'Sorry, you lost. The number was {num_gen}')
+                    break
+                else:
+                    print(f'wrong guess, try again. You only have {num_opor} chances left')
+        else:
+            print(f'Please, enter a number between {lim_inf}-{lim_sup}: ')
+    except ValueError:
+        print('enter a valid number')
+        continue
+
